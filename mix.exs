06676b4f230e7d267f5cf286dafbdf090420a1bd
Petr Stepchenko <nitrinodev@gmail.com>
2017-02-18 17:29:28 +0700

    fix mix.exs

--- mix.exs
+++ mix.exs
@@ -2,25 +2,20 @@ defmodule Money.Mixfile do
   use Mix.Project
 
   @version "1.2.2"
-  @github_url "https://github.com/Nitrino/monetize"
+  @github_url "https://github.com/Nitrino/elixir_money"
 
   def project do
-    [app: :money,
-     name: "monetize",
-     version: @version,
-     elixir: "~> 1.0",
-     deps: deps(),
-     source_url: @github_url,
-     docs: fn ->
-       [source_ref: "v#{@version}",
-        canonical: "http://hexdocs.pm/monetize",
-        main: "Money",
-        source_url: @github_url,
-        extras: ["README.md", "CONTRIBUTING.md"]
-       ]
-     end,
-     description: description(),
-     package: package()]
+    [
+      app: :elixir_money,
+      name: "Money",
+      version: @version,
+      elixir: "~> 1.0",
+      deps: deps(),
+      source_url: @github_url,
+      docs: docs(),
+      description: description(),
+      package: package(),
+    ]
   end
 
   def application do
@@ -38,13 +33,25 @@ defmodule Money.Mixfile do
 
       # Docs
       {:ex_doc, "~> 0.14", only: [:dev, :docs]},
+      {:remix, "~> 0.0.1", only: :dev}
     ]
   end
 
   defp description do
     """
-    Elixir library for easy working with Money. It is fork and development of library [money](https://github.com/liuggio/money)
+    Elixir library for easy working with Money.
+    It is fork and development of library [money](https://github.com/liuggio/money)
     """
+  end
+
+  defp docs do
+    [
+      source_ref: "v#{@version}",
+      canonical: "http://hexdocs.pm/elixir_money",
+      main: "Money",
+      source_url: @github_url,
+      extras: ["README.md", "CONTRIBUTING.md"]
+    ]
   end
 
   defp package do
